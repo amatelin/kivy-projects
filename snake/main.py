f3a0633275cf43ae8d7cd0481b560bb4b3969277
Alexis Matelin <a.matelin@gmail.com>
2014-12-29 18:37:15 -0500

    Commit 9

--- snake/main.py
+++ snake/main.py
@@ -1,19 +1,25 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import OptionProperty, ObjectProperty
+from kivy.properties import OptionProperty, ObjectProperty, NumericProperty
 from kivy.clock import Clock
 
 class SnakeHead(Widget):
     direction = OptionProperty("Right", options=["Up", "Down", "Left", "Right"])
+    velocity = NumericProperty(0)
 
     def move(self):
-        self.x += 10
+        self.x += self.velocity
 
 class SnakeGame(Widget):
     snake = ObjectProperty(None)
 
     def update(self, dt):
         self.snake.move()
+
+        # out of bounds
+        if self.snake.x < self.x or self.snake.x > self.width \
+        or self.snake.y < self.y or self.snake.y > self.height:
+            self.snake.velocity *= -1
 
 
 
@@ -23,7 +29,7 @@ class SnakeApp(App):
 
     def build(self):
         game = SnakeGame()
-        Clock.schedule_interval(game.update, 1.0 / 2)
+        Clock.schedule_interval(game.update, 1.0 / 5)
         return game
 
 

20420ca0be7997e791dcf1f05a5031a5fe932b72
Alexis Matelin <a.matelin@gmail.com>
2014-12-28 23:46:07 -0500

    Commit 4

--- linux/main.py
+++ linux/main.py
@@ -1,25 +1,52 @@
-import kivy
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.properties import \
+    ObjectProperty, \
+    NumericProperty, \
+    ReferenceListProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
-class LoginScreen(GridLayout):
-    
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='Password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
 
-class MyApp(App):
-    
+class PongBall(Widget):
+
+    # velocity of the ball on x and y axis
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.centrer = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 300))
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce of top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        # boucle off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
+
+class PongApp(App):
+
     def build(self):
-        return LoginScreen()
-        
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
+
+
 if __name__ == "__main__":
-    MyApp().run()
+    PongApp().run()

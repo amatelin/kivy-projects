7146fc1787282c472c4d7104244e6b9b748d14bb
Alexis Matelin <a.matelin@gmail.com>
2014-12-29 20:40:03 -0500

    Commit 10

--- snake/main.py
+++ snake/main.py
@@ -1,17 +1,59 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import OptionProperty, ObjectProperty, NumericProperty
+from kivy.properties import OptionProperty, ObjectProperty, NumericProperty, ReferenceListProperty, ListProperty
 from kivy.clock import Clock
+from kivy.vector import Vector
 
 class SnakeHead(Widget):
     direction = OptionProperty("Right", options=["Up", "Down", "Left", "Right"])
-    velocity = NumericProperty(0)
+    points = ListProperty([0]*6)
+    x_position = NumericProperty(0)
+    y_position = NumericProperty(1)
+    position = ReferenceListProperty(x_position, y_position)
 
     def move(self):
-        self.x += self.velocity
+        if self.direction=="Right":
+            self.position[0]+=1
+            x0 = self.position[0]*self.width
+            y0 = self.position[1]*self.height/2
+            x1 = x0-self.width
+            y1 = y0+self.height/2
+            x2 = x0-self.width
+            y2 = y0-self.height/2
+        elif self.direction=="Left":
+            self.position[0]-=1
+            x0 = (self.position[0]-1)*self.width
+            y0 = self.position[1]*self.height/2
+            x1 = x0+self.width
+            y1 = y0-self.height/2
+            x2 = x0+self.width
+            y2 = y0+self.height/2  
+        elif self.direction=="Up":
+            self.position[1]+=1
+            x0 = self.position[0]*self.width/2
+            y0 = self.position[1]*self.height
+            x1 = x0-self.width/2
+            y1 = y0-self.height
+            x2 = x0+self.width/2
+            y2 = y0-self.height  
+        elif self.direction=="Down":
+            self.position[1]-=1
+            x0 = self.position[0]*self.width/2
+            y0 = (self.position[1]-1)*self.height
+            x1 = x0+self.width/2
+            y1 = y0+self.height
+            x2 = x0-self.width/2
+            y2 = y0+self.height 
+
+        self.points = [x0, y0, x1, y1, x2, y2]
 
 class SnakeGame(Widget):
     snake = ObjectProperty(None)
+    score = NumericProperty(0)
+
+    def new_snake(self):
+        pass
+
 
     def update(self, dt):
         self.snake.move()
@@ -19,7 +61,7 @@ class SnakeGame(Widget):
         # out of bounds
         if self.snake.x < self.x or self.snake.x > self.width \
         or self.snake.y < self.y or self.snake.y > self.height:
-            self.snake.velocity *= -1
+            self.direction = "Right"
 
 
 
@@ -29,7 +71,7 @@ class SnakeApp(App):
 
     def build(self):
         game = SnakeGame()
-        Clock.schedule_interval(game.update, 1.0 / 5)
+        Clock.schedule_interval(game.update, 1.0 / 1)
         return game
 
 

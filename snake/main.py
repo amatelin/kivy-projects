d038fe214289cac798699b4407daa1ad455ce2b4
Alexis Matelin <a.matelin@gmail.com>
2015-01-01 18:35:48 -0500

    Commit 17

--- snake/main.py
+++ snake/main.py
@@ -9,7 +9,7 @@ from kivy.properties import \
     BooleanProperty
 from kivy.clock import Clock
 from kivy.vector import Vector
-from kivy.graphics import Rectangle, Ellipse
+from kivy.graphics import Rectangle, Ellipse, Triangle
 from random import randint
 
 
@@ -22,6 +22,9 @@ class Snake(Widget):
         self.head.move()
         self.tail.add_block(next_tail_pos)
 
+    def remove(self):
+        self.tail.remove()
+
     def set_next_direction(self, direction):
         self.head.direction = direction
 
@@ -30,7 +33,13 @@ class SnakeTail(Widget):
     size = NumericProperty(3)
     blocks_positions = ListProperty()
     tail_blocks = ListProperty()
-    block = ObjectProperty(None)
+
+    def remove(self):
+        self.size = 3
+        for block in self.tail_blocks:
+            self.canvas.remove(block)
+        self.tail_blocks = []
+        self.blocks_positions = []
 
     def add_block(self, pos):
         self.blocks_positions.append(pos)
@@ -52,10 +61,24 @@ class SnakeTail(Widget):
 class SnakeHead(Widget):
     direction = OptionProperty(
         "Right", options=["Up", "Down", "Left", "Right"])
-    points = ListProperty([0] * 6)
+    points = ListProperty([50] * 6)
     x_position = NumericProperty(10)
     y_position = NumericProperty(1)
     position = ReferenceListProperty(x_position, y_position)
+    object_on_board = ObjectProperty(None)
+    state = BooleanProperty(False)
+
+    def is_on_board(self):
+        return self.state
+
+    def show(self):
+        with self.canvas:
+            if not self.is_on_board():
+                self.object_on_board = Triangle(points=self.points)
+                self.state = True
+            else:
+                self.canvas.remove(self.object_on_board)
+                self.object_on_board = Triangle(points=self.points)            
 
     def move(self):
         print "Rendered {0} at {1}".format(self.direction, self.position)
@@ -93,36 +116,65 @@ class SnakeHead(Widget):
             y2 = y0 + self.height
 
         self.points = [x0, y0, x1, y1, x2, y2]
-
+        self.show()
 
 class Fruit(Widget):
-    image = ObjectProperty(None)
-    duration = NumericProperty(2)
+    object_on_board = ObjectProperty(None)
+    state = BooleanProperty(False)
+    duration = NumericProperty(15)
     interval = NumericProperty(3)
 
     def pop(self, pos):
+        self.pos = pos
         with self.canvas:
             x = (pos[0] - 1) * self.size[0]
             y = (pos[1] - 1) * self.size[1]
             coord = (x, y)
-            self.image = Ellipse(pos=coord, size=self.size)
+            self.state = True
+            self.object_on_board = Ellipse(pos=coord, size=self.size)
 
     def remove(self):
-        self.canvas.remove(self.image)
-        self.image = ObjectProperty(None)
+        self.canvas.remove(self.object_on_board)
+        self.state = False
+        self.object_on_board = ObjectProperty(None)
 
+    def is_on_board(self):
+        return self.state
 
 class SnakeGame(Widget):
     snake = ObjectProperty(None)
+
     fruit = ObjectProperty(None)
-    fruit_rythme = NumericProperty(0)
+    fruit_ythme = NumericProperty(0)
+
     turn_counter = NumericProperty(0)
+    time_coeff = NumericProperty(1)
     score = NumericProperty(0)
+
     mov_start_pos = ListProperty()
     mov_current_pos = ListProperty()
     mov_triggered = BooleanProperty(False)
+
     col_number = 20
     row_number = 10
+
+    def start(self):
+        print "Start"
+        self.new_snake()
+        self.update()
+
+    def reset(self):
+        self.turn_counter = 0
+        self.time_coeff = 1
+        self.score = 0
+
+        self.snake.remove()
+        if self.fruit.is_on_board():
+            self.remove_fruit()
+
+        Clock.unschedule(self.pop_fruit)
+        Clock.unschedule(self.remove_fruit)
+        Clock.unschedule(self.update)
 
     def new_snake(self):
         start_position = (
@@ -144,32 +196,43 @@ class SnakeGame(Widget):
         print "Poped ! at {}".format(coord)
 
     def remove_fruit(self, *args):
-        if self.fruit.image:
+        if self.fruit.is_on_board:
             self.fruit.remove()
 
-    def update(self, dt):
+    def update(self, *args):
         if self.turn_counter == 0:
-            print "A"
             self.fruit_rythme = self.fruit.interval + self.fruit.duration
             Clock.schedule_interval(self.remove_fruit, self.fruit_rythme / 1)
         elif self.turn_counter == self.fruit.interval:
-            print "B"
             self.pop_fruit()
             Clock.schedule_interval(self.pop_fruit, self.fruit_rythme / 1)
 
         self.snake.move()
         snake_position = self.snake.head.position
 
+        if self.fruit.is_on_board():
+            if snake_position == self.fruit.pos:
+                self.score += 1
+                self.snake.tail.size += 1
+                self.time_coeff *= 1.05
+
         if snake_position in self.snake.tail.blocks_positions:
             print "LOOOSER"
+            self.reset()
+            self.start()
+            return
 
         if snake_position[0] > self.col_number \
                 or snake_position[0] < 1 \
                 or snake_position[1] > self.row_number \
                 or snake_position[1] < 1:
             print "LOOOOOSER AGAIN"
-
+            self.reset()
+            self.start()
+            return
+        
         self.turn_counter += 1
+        Clock.schedule_once(self.update, 1/self.time_coeff)
 
     def on_touch_down(self, touch):
         self.mov_start_pos = touch.spos
@@ -182,7 +245,7 @@ class SnakeGame(Widget):
 
         delta = Vector(*self.mov_current_pos) - Vector(*self.mov_start_pos)
         if (self.mov_triggered == False) \
-                and (abs(delta[0]) > 0.15 or abs(delta[1]) > 0.20):
+                and (abs(delta[0]) > 0.1 or abs(delta[1]) > 0.1):
             if abs(delta[0]) > abs(delta[1]):
                 if delta[0] > 0:
                     self.snake.set_next_direction("Right")
@@ -201,8 +264,8 @@ class SnakeApp(App):
 
     def build(self):
         game = SnakeGame()
-        game.new_snake()
-        Clock.schedule_interval(game.update, 1.0 / 1)
+        game.start()
+        # Clock.schedule_interval(game.update, 1.0 / game.time_coeff)
         return game
 
 
